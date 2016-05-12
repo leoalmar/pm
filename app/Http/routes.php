043ac48735e8e8b1197cb97bd49d86d605259f7e
Leonardo Almeida Araújo <leonardoxsystems@gmail.com>
2016-05-12 16:55:49 -0300

    Refresh Token

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,23 +6,28 @@ Route::get('/', function () {
 });
 
 
-Route::get('client','ClientsController@index');
-Route::post('client','ClientsController@store');
-Route::get('client/{id}','ClientsController@show');
-Route::put('client/{id}','ClientsController@update');
-Route::delete('client/{id}','ClientsController@destroy');
 
-Route::get('project/{id}/note','ProjectNotesController@index');
-Route::post('project/{id}/note','ProjectNotesController@store');
-Route::get('project/{id}/note/{noteId}','ProjectNotesController@show');
-Route::put('project/{id}/note/{noteId}','ProjectNotesController@update');
-Route::delete('project/{id}/note/{noteId}','ProjectNotesController@destroy');
+Route::group(['middleware' => 'oauth'], function(){
 
-Route::get('project','ProjectsController@index');
-Route::post('project','ProjectsController@store');
-Route::get('project/{id}','ProjectsController@show');
-Route::put('project/{id}','ProjectsController@update');
-Route::delete('project/{id}','ProjectsController@destroy');
+    Route::get('client','ClientsController@index');
+    Route::post('client','ClientsController@store');
+    Route::get('client/{id}','ClientsController@show');
+    Route::put('client/{id}','ClientsController@update');
+    Route::delete('client/{id}','ClientsController@destroy');
+
+    Route::get('project/{id}/note','ProjectNotesController@index');
+    Route::post('project/{id}/note','ProjectNotesController@store');
+    Route::get('project/{id}/note/{noteId}','ProjectNotesController@show');
+    Route::put('project/{id}/note/{noteId}','ProjectNotesController@update');
+    Route::delete('project/{id}/note/{noteId}','ProjectNotesController@destroy');
+
+    Route::get('project','ProjectsController@index');
+    Route::post('project','ProjectsController@store');
+    Route::get('project/{id}','ProjectsController@show');
+    Route::put('project/{id}','ProjectsController@update');
+    Route::delete('project/{id}','ProjectsController@destroy');
+});
+
 
 
 
